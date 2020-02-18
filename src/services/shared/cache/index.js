5d8721e51da97db638e9e63d2731be8ee8c38cb6
Luiz Felippe Vieira de Andrade | ELS INFORMACAO <luiz.andrade_ext@stone.com.br>
2020-02-18 13:14:00 -0300

    fix message

--- src/services/shared/cache/index.js
+++ src/services/shared/cache/index.js
@@ -65,7 +65,7 @@ const del = (key) => {
       }
       return resolve({
         code: response,
-        message: 'Registro removido com sucesso'
+        message: 'Unregistered successfully'
       })
     })
   })
